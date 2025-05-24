c11d7ae9f5aae378f3526f3426b9b2e17036be57
Darren Sim <e0725166@u.nus.edu>
2025-05-24 17:46:18 +0800

    Make lobby code copyable

--- Assets/MyAssets/Scripts/UI/LobbyCodeGrabber.cs
+++ Assets/MyAssets/Scripts/UI/LobbyCodeGrabber.cs
@@ -3,7 +3,9 @@ using TMPro;
 public class LobbyCodeGrabber : MonoBehaviour
 {
 
+    private string lobbyCode;
     private TMP_Text lobbyCodeText;
+    [SerializeField] private GameObject copiedUI;
 
     public void Awake()
     {
@@ -13,6 +15,34 @@ public class LobbyCodeGrabber : MonoBehaviour
             lobbyCodeText.text = "";
             return; // Dev build
         }
-        lobbyCodeText.text = $"Lobby Id: {SteamLobby.instance.LobbyCode}";
+        lobbyCode = SteamLobby.instance.LobbyCode;
+        lobbyCodeText.text = $"Lobby Code: {lobbyCode}";
+    }
+
+    public void CopyCodeToClipboard()
+    {
+        if (SteamLobby.instance == null)
+        {
+            Debug.Log("No lobby code to copy");
+            return;
+        }
+        GUIUtility.systemCopyBuffer = lobbyCode;
+        ShowCopiedUI();
+    }
+
+    private void ShowCopiedUI()
+    {
+        if (copiedUI != null)
+        {
+            copiedUI.SetActive(true);
+            Invoke(nameof(HideCopiedUI), 2f); // Hide after 2 seconds
+        }
+    }
+    private void HideCopiedUI()
+    {
+        if (copiedUI != null)
+        {
+            copiedUI.SetActive(false);
+        }
     }
 }
