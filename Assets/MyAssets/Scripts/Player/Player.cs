06a78347140295612eb22fd214c047ec1614417d
Darren Sim <e0725166@u.nus.edu>
2025-05-26 00:17:05 +0800

    Unsteam

--- Assets/MyAssets/Scripts/Player/Player.cs
+++ Assets/MyAssets/Scripts/Player/Player.cs
@@ -107,17 +107,17 @@ public class Player : NetworkBehaviour
     public void GetSteamUsername()
     {
         string username;
-        if (SteamManager.Initialized)
-        {
-            Debug.Log("Getting Steam username for player");
-            username = SteamFriends.GetPersonaName();
-            CmdUpdateSteamUsername(username);
-        }
-        else
-        {
-            username = "Player " + UnityEngine.Random.Range(0, 1000);
-            CmdUpdateSteamUsername(username);
-        }
+        // if (SteamManager.Initialized)
+        // {
+        //     Debug.Log("Getting Steam username for player");
+        //     username = SteamFriends.GetPersonaName();
+        //     CmdUpdateSteamUsername(username);
+        // }
+        // else
+        // {
+        username = "Player " + UnityEngine.Random.Range(0, 1000);
+        CmdUpdateSteamUsername(username);
+        // }
     }
 
     [Server]
